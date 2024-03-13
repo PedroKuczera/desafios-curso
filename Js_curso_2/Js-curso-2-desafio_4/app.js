2c9ec6e82451a61a60b7b3b9c3d5cc8a7bafdc8b
Pedro Kuczera <pedrohkd001@gmail.com>
2024-03-13 11:39:58 -0300

    Desafios

---
+++ Js_curso_2/Js-curso-2-desafio_4/app.js
@@ -0,0 +1,20 @@
+// 1 - Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica [];
+
+// 2 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push['Java', 'Ruby', 'GoLang'];
+
+// 4 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+// 5 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento
+let nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+// 6 - Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
